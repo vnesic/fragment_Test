b050e37e307d46a6411678ff4fefbc34450af19a
vnesic <vladimirs.nesic@gmail.com>
2016-12-11 19:33:59 +0100

    m...

--- app/src/main/java/com/example/user/fragment_test/SettingActivity.java
+++ app/src/main/java/com/example/user/fragment_test/SettingActivity.java
@@ -3,6 +3,12 @@ package com.example.user.fragment_test;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 /**
  * Created by Buljoslav on 10/12/2016.
@@ -11,11 +17,79 @@ import android.os.PersistableBundle;
 public class SettingActivity extends Activity {
 
 
+    private Button backButton,backButtonDummy;
+    int width=0;
+    int height=0;
+
+    private TextView title,backgroundColor,textColor,textSize;
+    private Button yellowColorButton,whiteColorButton,yellow2ColorButton,whiteColor2Button;
+    private Button textBlack,textBrown,textBlue,textRed;
+    private Button dummyButton,sizeUPButton,sizeDownButton;
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        this.setContentView(R.layout.activity_main1);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.settings_layout);
 
+        WindowManager w = getWindowManager();
+        Display d = w.getDefaultDisplay();
 
+        width = d.getWidth();
+        height = d.getHeight();
+
+        init();
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        backButtonDummy=(Button)findViewById(R.id.backButtonDummy);
+        arrangeLayout();
+
+    }
+
+    void arrangeLayout(){
+
+        backButton.setWidth((int) (width * 0.15));
+        backButtonDummy.setWidth((int) (width * 0.15));
+        title.setWidth((int) (width * 0.6));
+
+        yellowColorButton.setWidth((int) (width * 0.15));
+        whiteColorButton.setWidth((int) (width * 0.15));
+        yellow2ColorButton.setWidth((int) (width * 0.15));
+        whiteColor2Button.setWidth((int) (width * 0.15));
+        textBlack.setWidth((int) (width * 0.15));;
+        textBrown.setWidth((int) (width * 0.15));
+        textBlue.setWidth((int) (width * 0.15));;
+        textRed.setWidth((int) (width * 0.15));
+ //       dummyButton.setWidth((int) (width * 0.3));
+   //     sizeUPButton.setWidth((int) (width * 0.25));
+   //     sizeDownButton.setWidth((int) (width * 0.25));
+
+        backgroundColor.setWidth((int) (width * 0.3));
+        textColor.setWidth((int) (width * 0.3));
+        textSize.setWidth((int) (width * 0.45));
+    }
+
+    void init(){
+
+        title=(TextView)findViewById(R.id.subtitleTitle);
+        backButton=(Button)findViewById(R.id.backButton);
+
+        yellowColorButton=(Button)findViewById(R.id.yellowColor);
+        whiteColorButton=(Button)findViewById(R.id.whiteColor);
+        yellow2ColorButton=(Button)findViewById(R.id.yellowColor2);
+        whiteColor2Button=(Button)findViewById(R.id.whiteColor2);
+        textBlack=(Button)findViewById(R.id.texBlack);
+        textBrown=(Button)findViewById(R.id.textBrown);
+        textBlue=(Button)findViewById(R.id.textBlue);
+        textRed=(Button)findViewById(R.id.textRed);
+   //     dummyButton=(Button)findViewById(R.id.dummyButton);
+        sizeUPButton=(Button)findViewById(R.id.sizeUP);
+        sizeDownButton=(Button)findViewById(R.id.sizeDOWN);
+        backgroundColor=(TextView)findViewById(R.id.backgroundColor);
+        textColor=(TextView)findViewById(R.id.textColor);
+        textSize=(TextView)findViewById(R.id.textSize);
     }
 }
